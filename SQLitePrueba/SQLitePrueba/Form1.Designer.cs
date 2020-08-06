a826bd253de16c2191a76169ac82cb318a4d8e16
MiGueAJM9724 <miguejm@outlook.es>
2020-08-05 23:37:55 -0500

    CRUDSQLiteCSharp

---
+++ SQLitePrueba/SQLitePrueba/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace SQLitePrueba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Grid = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Grid
+            // 
+            this.Grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Grid.Location = new System.Drawing.Point(12, 12);
+            this.Grid.Name = "Grid";
+            this.Grid.RowHeadersWidth = 51;
+            this.Grid.RowTemplate.Height = 24;
+            this.Grid.Size = new System.Drawing.Size(490, 150);
+            this.Grid.TabIndex = 0;
+            this.Grid.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.Grid_CellClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 184);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID Producto :";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(129, 181);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 22);
+            this.txtID.TabIndex = 2;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(252, 215);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(130, 23);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Buscar";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(337, 184);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(147, 22);
+            this.txtName.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(269, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nombre:";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(78, 215);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(147, 22);
+            this.txtPrice.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Precio:";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(37, 254);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(130, 23);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Guardar";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(191, 254);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(130, 23);
+            this.btnUpdate.TabIndex = 9;
+            this.btnUpdate.Text = "Actualizar";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(354, 254);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(130, 23);
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Text = "Borrar";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 428);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Grid);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Grid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView Grid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ SQLitePrueba/SQLitePrueba/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace SQLitePrueba
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private SQLiteConnection SQL_Cn;
+        private SQLiteCommand SQL_Command;
+        private SQLiteDataAdapter DB;
+        private DataSet DS = new DataSet();
+        private DataTable DT = new DataTable();
+
+        private void SetConnection()
+        {
+            SQL_Cn = new SQLiteConnection("Data Source = prueba.db; Version = 3; New = False; Compress = True;");
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+        private void ExecuteQuery(string Query)
+        {
+            SetConnection();
+            SQL_Cn.Open();
+            SQL_Command = SQL_Cn.CreateCommand();
+            SQL_Command.CommandText = Query;
+            SQL_Command.ExecuteNonQuery();
+            SQL_Cn.Close();
+
+        }
+
+        private void LoadData()
+        {
+            SetConnection();
+            SQL_Cn.Open();
+            SQL_Command = SQL_Cn.CreateCommand();
+            string Query = "SELECT * FROM Producto";
+            DB = new SQLiteDataAdapter(Query, SQL_Cn);
+            DS.Reset();
+            DB.Fill(DS);
+            DT = DS.Tables[0];
+            Grid.DataSource = DT;
+            SQL_Cn.Close();
+
+        }
+        // button Save (add)
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text == "" || txtPrice.Text == "")
+            {
+                MessageBox.Show("Llene el formulario");
+            }
+            else
+            {
+                string Query = "INSERT INTO Producto(nombre, precio) VALUES('" + txtName.Text + "', " + txtPrice.Text + ")";
+                ExecuteQuery(Query);
+                LoadData();
+            }
+                
+        }
+
+        private void Grid_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtID.Text = Grid.SelectedRows[0].Cells[0].Value.ToString();
+            txtName.Text = Grid.SelectedRows[0].Cells[1].Value.ToString();
+            txtPrice.Text = Grid.SelectedRows[0].Cells[2].Value.ToString();
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text == "" || txtPrice.Text == "")
+            {
+                MessageBox.Show("Llene el formulario");
+            }
+            else
+            {
+                string Query = "UPDATE Producto SET nombre ='" + txtName.Text + "', precio = " + txtPrice.Text + " WHERE id_producto = " + txtID.Text + "";
+                ExecuteQuery(Query);
+                LoadData();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (txtID.Text == "")
+            {
+                MessageBox.Show("Inserte un ID");
+            } 
+            else
+            {
+                string Query = "DELETE FROM Producto WHERE id_producto = " + txtID.Text + "";
+                ExecuteQuery(Query);
+                LoadData();
+                txtID.Text = "";
+                txtName.Text = "";
+                txtPrice.Text = "";
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if(txtID.Text == "")
+            {
+                MessageBox.Show("Ingrese el ID a buscar");
+            }
+            else
+            {
+                SetConnection();
+                SQL_Cn.Open();
+                string Query = "SELECT  nombre, precio FROM Producto WHERE id_producto = " + txtID.Text + "";
+                SQLiteCommand cmd = new SQLiteCommand(Query, SQL_Cn);
+                SQLiteDataReader lector = cmd.ExecuteReader();
+                if (lector.Read())
+                {
+                    txtName.Text = lector[0].ToString();
+                    txtPrice.Text = lector[1].ToString();
+                }
+                else MessageBox.Show("Error al ejecutar consulta");
+            }
+        }
+    }
+}
